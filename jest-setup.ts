fa8f21e6ae071f81a017e24c865dec83766e2a9d
YuriMorning7 <Lloydeast1@gmail.com>
2025-10-05 09:19:32 +0800

    fix: dependency

--- jest-setup.ts
+++ jest-setup.ts
@@ -1,7 +1,13 @@
 import '@testing-library/react-native/extend-expect';
 
-// react-hook form setup for testing
+// react-hook-form setup for testing
 // @ts-ignore
 global.window = {};
 // @ts-ignore
 global.window = global;
+
+// Mock expo-localization for tests
+jest.mock('expo-localization', () => ({
+  getLocales: () => [{ languageTag: 'en-US', isRTL: false }],
+  locale: 'en-US',
+}));
--- src/lib/i18n/index.tsx
+++ src/lib/i18n/index.tsx
@@ -1,4 +1,4 @@
-import { locale } from 'expo-localization';
+import * as Localization from 'expo-localization';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import { I18nManager } from 'react-native';
@@ -7,19 +7,18 @@ import { resources } from './resources';
 import { getLanguage } from './utils';
 export * from './utils';
 
+const currentLocale = Localization.getLocales()[0].languageTag;
+
 i18n.use(initReactI18next).init({
   resources,
-  lng: getLanguage() || locale, // TODO: if you are not supporting multiple languages or languages with multiple directions you can set the default value to `en`
+  lng: getLanguage() || currentLocale,
   fallbackLng: 'en',
-  compatibilityJSON: 'v3', // By default React Native projects does not support Intl
-
-  // allows integrating dynamic values into translations.
+  compatibilityJSON: 'v3',
   interpolation: {
-    escapeValue: false, // escape passed in values to avoid XSS injections
+    escapeValue: false,
   },
 });
 
-// Is it a RTL language?
 export const isRTL: boolean = i18n.dir() === 'rtl';
 
 I18nManager.allowRTL(isRTL);
